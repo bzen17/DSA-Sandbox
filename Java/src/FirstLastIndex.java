f237cb965186a0019764ba66583eebaae2022cd9
Ayan Banerjee <ayanbanerjee95@outlook.com>
2023-07-08 19:31:44 +0530

    First and Last Index of target in array

---
+++ Java/src/FirstLastIndex.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class FirstLastIndex {
+    public static void main(String[] args) {
+        int[] arr = { 1, 1, 2, 3, 5, 5, 5, 5, 5, 5, 7, 7, 7, 7, 7, 8, 8 };
+        int target = 7;
+        System.out.println(Arrays.toString(arr));
+        Solution(arr, target);
+
+    }
+
+    public static void Solution(int[] arr, int target) {
+        int first = -1, last = -1;
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                if (first != -1) {
+                    if (arr[mid + 1] > target) {
+                        last = mid;
+                        System.out.println(first + " " + last);
+                        break;
+                    } else {
+                        left = mid + 1;
+                    }
+                } else {
+                    if (arr[mid - 1] < target) {
+                        first = mid;
+                        left = mid + 1;
+                        right = arr.length - 1;
+                    } else {
+                        right = mid - 1;
+                    }
+                }
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+}
---
+++ Python/FirstLastIndex.py
@@ -0,0 +1,35 @@
+def FirstLastIndex():
+    arr = [1, 1, 2, 3, 5, 5, 5, 5, 5, 5, 7, 7, 7, 7, 7, 8, 8]
+    target = 7
+    Solution(arr, target)
+
+
+def Solution(arr, target):
+    first = last = -1
+    left = 0
+    right = len(arr) - 1
+    print(arr)
+    while (left <= right):
+        mid = int(left + (right-left)/2)
+        if (arr[mid] == target):
+            if (first == -1):
+                if (arr[mid-1] < target):
+                    first = mid
+                    left = mid + 1
+                    right = len(arr) - 1
+                else:
+                    right = mid - 1
+            else:
+                if (arr[mid+1] > target):
+                    last = mid
+                    print("{} {}".format(first, last))
+                    break
+                else:
+                    left = mid + 1
+        elif (arr[mid] < target):
+            left = mid + 1
+        else:
+            right = mid - 1
+
+
+FirstLastIndex()
